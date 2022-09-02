45a88e36b9c8a23dd4c156067d386ed9fed62a12
DESKTOP-SURDNHS\jrami <jramiroquaii@gmail.com>
2022-09-02 01:58:53 -0700

    updates to deploy to heroku

--- server.js
+++ server.js
@@ -2,8 +2,5 @@ const express = require('express');
 const path = require('path');
 const app = express();
 app.use(express.static(__dirname + '/dist/usuarios-v2'));
-app.get('/*', function (req, res) {
-    res.sendFile(path.join(__dirname +
-        '/dist/usuarios-v2/index.html'));
-});
+app.get('/*', function(req,res){res.sendFile(path.join(__dirname+'/dist/usuarios-v2/index.html'));});
 app.listen(process.env.PORT || 8080);
